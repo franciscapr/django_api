e0b079479b3c51ecfb360e43c05d7f231ba1c0ad
Francisca <francisca.paredes.pr@gmail.com>
2025-03-10 11:07:59 -0300

    RegisterViewSet

--- core/routers.py
+++ core/routers.py
@@ -1,7 +1,10 @@
 from rest_framework import routers
 from core.user.viewsets import UserViewSet
-
+from core.auth.viewsets.register import RegisterViewSet
 router = routers.SimpleRouter()
+
+########## AUTH ##########
+router.register(r'auth/register', RegisterViewSet, basename='auth-register')
 
 ########## USER ##########
 
