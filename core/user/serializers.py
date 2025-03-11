c49cae8be9e25496d5e944a6abf397f200391d3b
Francisca <francisca.paredes.pr@gmail.com>
2025-03-11 09:28:04 -0300

    UserSerializer hereda de AbstractSerializer

--- core/user/serializers.py
+++ core/user/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from core.user.models import User
+from core.abstract.serializer import AbstractSerializer
 
-class UserSerializer(serializers.ModelSerializer):
+
+class UserSerializer(AbstractSerializer):
     id = serializers.UUIDField(source='public_id', read_only=True, format='hex')
     created = serializers.DateTimeField(read_only=True)
     updated = serializers.DateTimeField(read_only=True)
