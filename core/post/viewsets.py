188a6d845fc90194055c0197c2a62f4af63f69fb
Francisca <francisca.paredes.pr@gmail.com>
2025-03-11 15:48:39 -0300

    viewsets.py like and remove_like

--- core/post/viewsets.py
+++ core/post/viewsets.py
@@ -4,6 +4,7 @@ from core.post.models import Post
 from core.post.serializers import PostSerializer
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.decorators import action
 
 
 class PostViewSet(AbstractViewSet):
@@ -25,3 +26,20 @@ class PostViewSet(AbstractViewSet):
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    @action(methods=['post'], detail=True)
+    def like(self, request, *args, **kwargs):
+        post = self.get_object()
+        user = self.request.user
+
+        user.like(post)
+        serializer = self.serializer_class(post)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    @action(methods=['post'], details=True)
+    def remove_like(self, request, *args, **kwargs):
+        post = self.get_objects()
+        user = self.request.user
+        user.remove_like(post)
+        serializer = self.serializer_class(post)
+        return Response(serializer.data, status=status.HTTP_200_OK)
