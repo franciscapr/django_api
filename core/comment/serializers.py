b2c45bc87032606add38ef34979aeb2f01713cd7
Francisca <francisca.paredes.pr@gmail.com>
2025-03-12 10:46:28 -0300

    serializer comment

--- core/comment/serializers.py
+++ core/comment/serializers.py
@@ -10,11 +10,28 @@ from core.post.models import Post
 
 class CommentSerializer(AbstractSerializer):
     author = serializers.SlugRelatedField(
-        queryset=User.objects.all(), slug_field='public_id'
+        queryset=User.objects.all(), slug_field='public_id'   
     )
     post = serializers.SlugRelatedField(
         queryset=Post.objects.all(), slug_field='public_id'
     )
+
+    def validate_author(self, value):
+        if self.context["request"].user != value:
+            raise ValidationError("You can`t create a post for another user.")
+        return value
+    
+    def validate_post(self, value):
+        if self.instance:
+            return self.instance.post
+        return value
+    
+    def update(self, instance, validated_data):
+        if not instance.edited:
+            validated_data['edited'] = True
+
+        instance = super().update(instance, validated_data)
+        return instance
 
     def to_representation(self, instance):
         rep= super().to_representation(instance)
