ad875495aebe21172f885127203db02a79220b71
Francisca <francisca.paredes.pr@gmail.com>
2025-03-14 10:41:56 -0300

    test register

--- core/auth/tests.py
+++ core/auth/tests.py
@@ -20,3 +20,17 @@ class TestAuthenticationViewSet:
         assert response.data['user']['username'] == user.username
         assert response.data['user']['email'] == user.email
 
+
+    @pytest.mark.django_db
+    def test_register(self, client):
+        data = {
+            "username": "johndoe",
+            "email": "johndoe@yopmail.com",
+            "password": "test_password",
+            "first_name": "John",
+            "last_name": "Doe"
+        }
+
+        response = client.post(self.endpoint + "register/", data)
+        
+        assert response.status_code == status.HTTP_201_CREATED
