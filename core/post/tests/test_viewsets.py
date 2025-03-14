1c39b4f3484d5c47d285288ed06f4751d4360fb6
Francisca <francisca.paredes.pr@gmail.com>
2025-03-14 11:31:00 -0300

    test_delete_anonymous

--- core/post/tests/test_viewsets.py
+++ core/post/tests/test_viewsets.py
@@ -61,3 +61,24 @@ class TestPostViewSet:
         assert response.data['id'] == post.public_id.hex
         assert response.data['body'] == post.body
         assert response.data['author']['id'] == post.author.public_id.hex
+
+    def test_create_anonymouse(self, client):
+        data = {
+            "body": "Test Post Body",
+            "author": "test_user"
+        }
+        response = client.post(self.endpoint, data)
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_update_anonymous(self, client, post):
+        data = {
+            "body": "Test Post Body",
+            "author": "test_user"
+        }
+
+        response = client.put(self. endpoint + str(post.public_id) + "/", data)
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_delete_anonymous(self, client, post):
+        response = client.delete(self.endpoint + str(post.public_id) + "/")
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
