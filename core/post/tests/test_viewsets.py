78e6298ec77aa801b57cca972f132637998ff968
Francisca <francisca.paredes.pr@gmail.com>
2025-03-14 11:25:59 -0300

    test_list_anonymous

--- core/post/tests/test_viewsets.py
+++ core/post/tests/test_viewsets.py
@@ -49,3 +49,15 @@ class TestPostViewSet:
         client.force_authenticate(user=user)
         response = client.delete(self.endpoint + str(post.public_id) + "/")
         assert response.status_code == status.HTTP_204_NO_CONTENT
+
+    def test_list_anonymous(self, client, post):
+        response = client.get(self.endpoint)
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['count'] == 1
+
+    def test_retrieve_anonymous(self, client, post):
+        response = client.get(self.endpoint + str(post.public_id) + "/")
+        assert response.status_code  == status.HTTP_200_OK
+        assert response.data['id'] == post.public_id.hex
+        assert response.data['body'] == post.body
+        assert response.data['author']['id'] == post.author.public_id.hex
--- core/post/viewsets.py
+++ core/post/viewsets.py
@@ -5,11 +5,11 @@ from core.post.serializers import PostSerializer
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import action
-
+from core.auth.permissions import UserPermission
 
 class PostViewSet(AbstractViewSet):
     http_method_names = ('post', 'get', 'put', 'delete')
-    permission_classes = [IsAuthenticated]
+    permission_classes = (UserPermission,)
     serializer_class = PostSerializer
 
     def get_queryset(self):
